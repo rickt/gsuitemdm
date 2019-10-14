a6eb61117e44dd8f7bf2b4a8740fa354ba532666
rick tait <rickt@rickt.org>
2019-10-14 23:00:10 +0000

    added config file

--- core.go
+++ core.go
@@ -9,8 +9,12 @@ import (
 )
 
 // Create a new G Suite MDM Service
-func New(ctx context.Context) *GSuiteMDMService {
+func New(ctx context.Context, file string) *GSuiteMDMService {
+	// Load in main configuration file and get a config struct
+	cf := loadConfigFile(file)
+
 	return &GSuiteMDMService{
+		c:   cf,
 		ctx: ctx,
 	}
 }
