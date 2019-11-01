d70612e8ef781cf71bfc414ef7f9286773876a04
rick tait <rickt@rickt.org>
2019-11-01 00:52:49 +0000

    added updatedb, updatesheet commands

--- mdmtool/commands.go
+++ mdmtool/commands.go
@@ -241,7 +241,36 @@ func addUpdateDatastoreCommand(mdmtool *kingpin.Application) {
 
 // Setup the "updatedb" command
 func (ld *UpdateDatastoreCommand) run(c *kingpin.ParseContext) error {
-	fmt.Printf("updatedb goes here\n")
+	var rb gsuitemdm.UpdateRequest
+
+	// Setup the request body
+	rb.Key = m.Config.APIKey
+
+	// Marshal the JSON
+	js, err := json.Marshal(rb)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Build the http request
+	req, err := http.NewRequest("POST", m.Config.UpdateDatastoreURL, bytes.NewBuffer(js))
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	// Create an http client
+	client := &http.Client{}
+
+	// Send the request and get a nice response
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
+
 	return nil
 }
 
@@ -257,7 +286,36 @@ func addUpdateSheetCommand(mdmtool *kingpin.Application) {
 
 // Setup the "updatesheet" command
 func (ld *UpdateSheetCommand) run(c *kingpin.ParseContext) error {
-	fmt.Printf("updatesheet goes here\n")
+	var rb gsuitemdm.UpdateRequest
+
+	// Setup the request body
+	rb.Key = m.Config.APIKey
+
+	// Marshal the JSON
+	js, err := json.Marshal(rb)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Build the http request
+	req, err := http.NewRequest("POST", m.Config.UpdateSheetURL, bytes.NewBuffer(js))
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	// Create an http client
+	client := &http.Client{}
+
+	// Send the request and get a nice response
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
+
 	return nil
 }
 
