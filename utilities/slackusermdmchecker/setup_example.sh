69b980f8df8e5ebf5a2ce8d574deb48af930e159
rick tait <rickt@rickt.org>
2020-01-07 22:42:42 +0000

    initial checkin of example slackusermdmchecker setup file

---
+++ utilities/slackusermdmchecker/setup_example.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+export FROM_ADDR="mdm-compliance@yourdomain.com"
+export FROM_NAME="Company MDM Compliance"
+export GSUITEMDMTOKEN="YOURGSUITEMDMTOKENGOESHERE"
+export MDMSTATUSURL="https://us-central1-PROJECTID.cloudfunctions.net/SearchDatastore"
+export RECIPIENTS_ADDR="mdm-compliance-report-list@yourdomain.com"
+export RECIPIENTS_NAME="MDM Compliance Report List"
+export SENDGRID_API_KEY="YOURSENDGRIDAPIKEYGOESHERE"
+export SLACKTOKEN="YOURSLACKTOKENGOESHERE"
+
+# EOF
