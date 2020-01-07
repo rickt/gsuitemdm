0c12a343b3c6377439ba1a483b8f4aa8717742f2
rick tait <rickt@rickt.org>
2020-01-07 23:43:52 +0000

    cleanup

--- cloudfunctions/directory/deploy.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-# change this to point to your own GCP project
-PROJECT="mdm-updater"
-
-go get -u github.com/rickt/gsuitemdm
-go build
-gcloud config set project $PROJECT
-gcloud functions deploy Directory --runtime go111 --trigger-http --env-vars-file env_directory.yaml
