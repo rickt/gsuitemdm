a11f15c639ebaac0288dcbfba6c8a419064e63d4
rick tait <rickt@rickt.org>
2020-01-25 00:01:01 +0000

    inital commit

---
+++ secrets.go
@@ -0,0 +1,38 @@
+package gsuitemdm
+
+//
+// GSuiteMDM GCP Secret Manager related code
+//
+
+import (
+	"context"
+	"errors"
+
+	secretmanager "cloud.google.com/go/secretmanager/apiv1beta1"
+	secretmanagerpb "google.golang.org/genproto/googleapis/cloud/secretmanager/v1beta1"
+)
+
+// Get a secret from Secret Manager
+func (mdms *GSuiteMDMService) GetSecret(ctx context.Context, sid string) (string, error) {
+	// Create a Secret Manager client
+	client, err := secretmanager.NewClient(ctx)
+	if err != nil {
+		return "", errors.New("Error creating Secret Manage client: " + err.Error())
+	}
+
+	// Build the Secret Manager request
+	smreq := &secretmanagerpb.AccessSecretVersionRequest{
+		Name: sid,
+	}
+
+	// Call the Secret Manager API and get the requested secret using its ID
+	smres, err := client.AccessSecretVersion(ctx, smreq)
+	if err != nil {
+		return "", errors.New("Error retrieving secret: " + err.Error())
+	}
+
+	// Return the specified secret
+	return string(smres.Payload.Data), nil
+}
+
+// EOF
